8d7dcea2cfe31fe11ba6f0210dcfe60fb8fd9b14
Kaja Anna <kaja.szokalska@gmail.com>
2022-01-31 15:29:29 +0100

    add NavBar basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import NavBar from './navigation/NavBar';
 import Welcome from './welcome-page/pages/Welcome';
 import Auth from './user/pages/Auth';
 import MyLibrary from './books/pages/MyLibrary';
@@ -8,10 +10,14 @@ import BorrowedBooks from './books/pages/BorrowedBooks';
 import AddNewBook from './books/pages/AddNewBook';
 import EditBookData from './books/pages/EditBookData';
 import BorrowBook from './books/pages/BorrowBook';
+import Footer from './navigation/Footer';
+
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
       My App:
       Routes:
@@ -26,6 +32,7 @@ function App() {
         <Route path="/borrow-book" element={<BorrowBook />}/>
       </Routes>
       </header>
+      <Footer />
     </div>
   );
 }
--- src/navigation/NavBar.tsx
+++ src/navigation/NavBar.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Menu from './Menu';
+import { ReactComponent as BookMark } from '../assets/bookMarkEl.svg'
+
+import './NavBar.scss';
 
 function NavBar() {
-    return (
-        <div>
-            NavBar
+  return (
+    <>
+      <div className="nav-bar">
+        <div className="nav-bar_elements">
+          <div className="nav-bar_hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="nav-bar_logo">MyLibrary</div>
         </div>
-    );
+      </div>
+      <div  className="nav-bar_book-mark">
+        <BookMark />
+      </div>
+      )
+    </>
+  );
 }
 
 export default NavBar;
