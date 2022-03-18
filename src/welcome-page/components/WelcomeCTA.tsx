e8c5541a496e556db6dafbdcf68ac6f82f5ff096
Kaja Anna <kaja.szokalska@gmail.com>
2022-03-18 11:50:07 +0100

    change WelcomePage

--- src/welcome-page/components/WelcomeCTA.tsx
+++ src/welcome-page/components/WelcomeCTA.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from '../../shared/Button';
+import AuthContext from '../../shared/contexts/authContext';
+
+import './WelcomeCTA.scss'
 
 function WelcomeCTA() {
-    return (
-        <div>
-            WelcomeCTA
-        </div>
-    );
+  const auth = useContext(AuthContext);
+  return (
+    <div className="welcomeCTA">
+      <p> We will help you keep track of borrowed and lent books.</p>
+      {auth && (
+        <Link to="/my-library">
+          <Button buttonText="My Library" />
+        </Link>
+      )}
+      {!auth && (
+        <Link to="/auth">
+          <Button buttonText="Register" />
+        </Link>
+      )}
+    </div>
+  );
 }
 
 export default WelcomeCTA;
--- src/welcome-page/pages/Welcome.tsx
+++ src/welcome-page/pages/Welcome.tsx
@@ -1,33 +1,22 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { ReactComponent as WelcomeBG } from '../../assets/WelcomeBg.svg';
-import Button from '../../shared/Button';
-import Card from '../../shared/Card';
-import AuthContext from '../../shared/contexts/authContext';
+import Blobs from '../../assets/welcomeBG_blobs.png'
+
+import WelcomeCTA from '../components/WelcomeCTA'
 
 import './Welcome.scss';
 
 function Welcome() {
-  const auth = useContext(AuthContext);
-
   return (
     <div className="welcome">
-      <div className="welcome__info">
-        <Card title="Welcome to MyLibray App" addContentPadding>
-          <div className="welcome__text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
-            eligendi maiores autem id aliquam cumque deleniti sunt, officia
-            ratione incidunt nesciunt blanditiis expedita consequuntur?
-          </div>
-          <div className="welcome__register">
-            {auth && <Link to="/my-library"><Button buttonText="My Library"/></Link>}
-            {!auth && <Link to="/auth"><Button buttonText="Register"/></Link>}
-            </div>
-          <div className="welcome__picture">
-            <WelcomeBG />
-          </div>
-        </Card>
-      </div>
+        <img src={Blobs} />
+        <div className='welcome__content'>
+        <div className="welcome__picture">
+          <WelcomeBG />
+        </div>
+        <div className='welcome__text'>
+        <WelcomeCTA />
+        </div>
+        </div>
     </div>
   );
 }
